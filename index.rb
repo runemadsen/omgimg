97c082bc739960b591e489282e30a4bed3d91a1b
Rune Skjoldborg Madsen <rune@runemadsen.com>
2011-04-12 14:44:29 -0400

    User login working with ActiveRecord

--- index.rb
+++ index.rb
@@ -3,10 +3,20 @@ require 'bundler'
 Bundler.require
 require 'models'
 
+enable :sessions
 mime_type :ttf, 'font/ttf'
 mime_type :woff, 'font/woff'
 
 get '/' do
   erb :login
+end
+
+post '/users/new' do
+  session[:user] = User.find_or_create_by_name(params[:name])
+  redirect '/today'
+end
+
+get '/today/?' do
+  erb :today
 end
 
