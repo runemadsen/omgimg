fef69645edd9701587a17d2da9f81b44699a1051
Rune Madsen <rune@runemadsen.com>
2012-08-24 22:15:36 -0400

    trying again

--- models.rb
+++ models.rb
@@ -1,19 +1,12 @@
-configure :development, :test do
-  set :database, 'sqlite://development.db'
-end
-
-configure :production do
-  db = URI.parse(ENV['DATABASE_URL'])
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
-end
-
+db = URI.parse(ENV['DATABASE_URL'])
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
+)
 
 class Discussion < ActiveRecord::Base
   has_one :image, :as => :commentable
